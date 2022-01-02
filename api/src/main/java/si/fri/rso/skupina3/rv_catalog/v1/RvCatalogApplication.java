f79d6a0f11c1acaae114052d4d9acbec38d757c7
Tadej Stanonik <ts6103@student.uni-lj.si>
2022-01-02 23:05:41 +0100

    K8S config

--- api/src/main/java/si/fri/rso/skupina3/rv_catalog/v1/RvCatalogApplication.java
+++ api/src/main/java/si/fri/rso/skupina3/rv_catalog/v1/RvCatalogApplication.java
@@ -1,8 +1,11 @@
 package si.fri.rso.skupina3.rv_catalog.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService("rv-catalog-service")
 public class RvCatalogApplication extends Application {
 }
